e010aa5ea5c2d00d7c655b95ad3c994e48710c36
Ziyi Zhang <ziyizhang@MacBook-Pro-2.local>
2022-04-04 17:11:17 -0400

    new

---
+++ part-3/App.js
@@ -0,0 +1,21 @@
+function App() {
+    return (
+      <div>
+        <Person
+          name="Matt Lane"
+          age= {20}
+          hobbies = {["tv",'movie','watch']}
+        />
+        <Person
+          name="David Lane"
+          age= {16}
+          hobbies = {["tv",'movie','watch']}
+        />
+        <Person
+          name="loco Lane"
+          age= {8}
+          hobbies = {["tv",'movie','watch']}
+        />
+      </div>
+    );
+  }
